3ffa048cd236e6014c11af46d6cf2088e02d6a0f
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-06-02 17:04:34 +0200

    Source: TileSpawner.cs

--- Assets/Source/TileSpawner.cs
+++ Assets/Source/TileSpawner.cs
@@ -6,12 +6,55 @@ public class TileSpawner : MonoBehaviour
 {
     DungeonManager dungeonManager;
 
-    private void Start()
+    private void Awake()
     {
         dungeonManager = FindObjectOfType<DungeonManager>();
         GameObject goFloor = Instantiate(dungeonManager.floorPrefab, transform.position, Quaternion.identity) as GameObject;
         goFloor.name = dungeonManager.floorPrefab.name;
         goFloor.transform.SetParent(dungeonManager.transform);
+
+        if (transform.position.x < dungeonManager.maxX)
+        {
+            dungeonManager.maxX = transform.position.x;
+        }
+
+        if (transform.position.x < dungeonManager.minX)
+        {
+            dungeonManager.minX = transform.position.x;
+        }
+
+        if (transform.position.y < dungeonManager.maxY)
+        {
+            dungeonManager.maxY = transform.position.y;
+        }
+
+        if (transform.position.y < dungeonManager.minY)
+        {
+            dungeonManager.minY = transform.position.y;
+        }
+    }
+
+    private void Start()
+    {
+        LayerMask envMask = LayerMask.GetMask("Wall", "Floor");
+        Vector2 hitSize = Vector2.one * 0.8f;
+        
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                Vector2 tarPos = new Vector2(transform.position.x + x, transform.position.y + y);
+                Collider2D hit = Physics2D.OverlapBox(tarPos, hitSize, 0, envMask);
+
+                if (!hit)
+                {
+                    // Add a wall
+                    GameObject goWall = Instantiate(dungeonManager.wallPrefab, tarPos, Quaternion.identity) as GameObject;
+                    goWall.name = dungeonManager.wallPrefab.name;
+                    goWall.transform.SetParent(dungeonManager.transform);
+                }
+            }
+        }
 
         Destroy(gameObject);
     }
