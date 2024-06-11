c1bfef3824d9ae71c2d3a8a035981dbbbedaa3e1
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-06-11 23:04:50 +0200

    Build Automation: Create BuildScript.cs

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class BuildScript
+{
+    public static void BuildProject()
+    {
+        string[] scenes = { "Assets/Scenes/MainScene.unity" };
+        string pathToBuild = "D:/Builds/EnterTheDungeons/UnityBuild.exe";
+
+        BuildPipeline.BuildPlayer(scenes, pathToBuild, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+}
