215472f3d239522d65cfd0d6ea0c78ab2da0e097
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-06-13 21:25:03 +0200

    Update BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -9,7 +9,7 @@ public class BuildScript
     public static void BuildProject()
     {
         string[] scenes = { "Assets/Scenes/MainScene.unity" };
-        string pathToBuild = "D:/Builds/EnterTheDungeons/UnityBuild.exe";
+        string pathToBuild = "D:/ProjectBuilds/EnterTheDungeons/UnityBuild.exe";
 
         BuildPipeline.BuildPlayer(scenes, pathToBuild, BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
